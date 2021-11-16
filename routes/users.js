6772227fb99ffd58d98d287bfae4a29a334c19c6
Nathan-Yennupiak <nathanyennupiak1@gmail.com>
2021-11-16 00:53:52 -0800

    Added Delete User route

--- routes/users.js
+++ routes/users.js
@@ -7,16 +7,26 @@ const router = express.Router()
 const users = []
 
 //All routes here start with /users
+
+//GET ALL USERS
 router.get('/', (req, res) => {
 
     res.send(users)
 })
 
+//CREATE A USER
 router.post("/", (req, res) => {
     const user = req.body
 
     users.push({...user, id: uuidv4() });
     res.send(`User with username: ${user.firstName} added to the database`)
+})
+
+//GET USER WITH id
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    const foundUser = users.find((user) => user.id === id)
+    res.send(foundUser)
 })
 
 
